7677ab544e8df8c16242d7fb9eb4756715bea819
GUSTAVO <mendezlutin@gmail.com>
2026-03-20 08:06:18 -0600

    modificacion en ruta web

--- routes/web.php
+++ routes/web.php
@@ -95,4 +95,6 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
      Route::delete('/catalogos/{catalogo}/paginas/{pagina}', [AdminCatalogo::class, 'destroyPage'])
     ->name('catalogs.paginas.destroy');
+
+    //ADMIN
 });
